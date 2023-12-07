1e97402a17f7957a147543347506f83c4197405d
1106 <sanders.gesture-0c@icloud.com>
2023-12-07 17:15:26 -0600

    create method

---
+++ src/assets/modules/shuffle.js
@@ -0,0 +1,12 @@
+export const shuffle = (arr, n) => {
+    let shuffled = arr.slice(0);
+    let selected = [];
+
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+
+    selected = shuffled.slice(0, n)
+    return selected
+}
