d89d3adcf3e4b5f491b503f7b8a55c68b98e107c
1106 <sanders.gesture-0c@icloud.com>
2023-12-07 14:07:41 -0600

    set categories

--- src/Context/ProductsContext.jsx
+++ src/Context/ProductsContext.jsx
@@ -13,7 +13,11 @@ function ProductProvider({ children }) {
   useEffect(() => {
     setProductList(productDB)
     setLoading(false)
+
+    const uniqueCategories = [...new Set(productList.map(product => product.category))]
+    setCategories(uniqueCategories)
   }, [])
+
 
   const data = {
     search,
