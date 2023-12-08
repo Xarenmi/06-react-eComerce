3fb34348133208bfd6ebc7f182a1e510507398ac
1106 <sanders.gesture-0c@icloud.com>
2023-12-08 13:01:29 -0600

    product load product unstyled

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import dancingJesus from '@/assets/Dancing-Jesus-resize.gif'
+import { useParams } from 'react-router-dom'
+import { useProductContext } from '@/Context/ProductsContext'
 
 const Product = () => {
+  const { productList } = useProductContext()
+  let { title } = useParams()
+  const thisProduct = productList.find((product) => product.title === title.replace(/%20/g, ' '));
+
   return (
-    <main>
-      <h1>PRODUCT SECTION</h1>
-      <h2>ON DEVELOPMENT</h2>
-      <img src={dancingJesus} alt="Dancing Jesus" />
+    <main className='product'>  
+      <p>{thisProduct.category}</p>
+      <h1>{thisProduct.title}</h1>
+      <p>{thisProduct.description}</p>
+      <p>{thisProduct.price}</p>
+      <p>{thisProduct.discountPercentage}</p>
+      <p>{thisProduct.rating}</p>
+      <p>{thisProduct.brand}</p>
+      {/*images, id?*/}
     </main>
   )
 }
