d5a8fa3822ddf675474450440b9532e538fbc9ea
1106 <sanders.gesture-0c@icloud.com>
2023-12-08 22:34:40 -0600

    Route adjustments

--- src/Routes/RouterIndex.jsx
+++ src/Routes/RouterIndex.jsx
@@ -1,23 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import {  useProductContext } from '@/Context/ProductsContext'
-import { AddProduct, Assistance, Cart, Home, Login, Category,  Loading, Product, Root } from '@/Pages';
+import { Routes, Route } from 'react-router-dom'
+import { AddProduct, Assistance, Cart, Category, Home, Login, Product, Search } from '@/Pages'
+import Root from './Root'
 
 export default function RouterIndex() {
-        const { loading } = useProductContext()
-        document.body.style.backgroundColor = loading ? '#72b7c2' : '#72b7c246'
-
     return (
         <Routes>
             <Route path="/" element={<Root />}>
                 <Route path="home" element={<Home />} />
                 <Route path="cart" element={<Cart />} />
                 <Route path="login" element={<Login />} />
+                <Route path="search/:name" element={<Search />} />
                 <Route path="sell" element={<AddProduct />} />
                 <Route path="help" element={<Assistance />} />
-                <Route path='loading' element={<Loading />} />
                 <Route path="product/:title" element={<Product />} />
                 <Route path="category/:name" element={<Category />} />
-                <Route index element={loading ? <Loading /> : <Home />} />
+                <Route index element={<Home />} />
             </Route>
         </Routes>
     );
