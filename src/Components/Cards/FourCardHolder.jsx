2ab4d78b5a8976ab15ff8aaf62f0039ef49ea972
1106 <sanders.gesture-0c@icloud.com>
2023-12-09 14:03:02 -0600

    adjusted styles

--- src/Components/Cards/FourCardHolder.jsx
+++ src/Components/Cards/FourCardHolder.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import Card from './Card'
 import './fourCardHolder.sass'
-import { shuffle }from '@/assets/modules/shuffle.js'
+import { shuffle } from '@/assets/modules/shuffle.js'
 import { useProductContext } from '@/Context/ProductsContext'
 
-const FourCardHolder = ({category}) => {
+const FourCardHolder = ({ category }) => {
 
   const { productList } = useProductContext()
   const categoryProducts = productList.filter((product) => product.category === category)
-  const randomProducts  = shuffle(categoryProducts, 4)
+  const randomProducts = shuffle(categoryProducts, 4)
   const selectedCategory = category ? category.replace(/-/g, ' ') : ''
-  
+
   return (
     <div className='fourCardHolder'>
-        <h2>{selectedCategory.toUpperCase()}</h2>
+      <h2>{selectedCategory.toUpperCase()}</h2>
+      <div className='fourCardHolder__container'>
         {randomProducts.map((product) => (
-      <Card key={product.id} id={product.id} className='fourCardHolder__card' />
-    ))}
+          <Card key={product.id} id={product.id} className='fourCardHolder__container__card' />
+        ))}
+      </div>
     </div>
   )
 }
