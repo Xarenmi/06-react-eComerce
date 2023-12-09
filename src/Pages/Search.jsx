8d8b72b0fd6f0f2ed7c78d01247677a0ac0637dc
1106 <sanders.gesture-0c@icloud.com>
2023-12-08 23:21:18 -0600

    container created

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -8,18 +8,19 @@ const Search = () => {
   let { name } = useParams()
 
   const products = productList.filter((product) => product.title.toUpperCase().includes(name.toUpperCase()))
-  console.log(products)
 
 
   return (
     <main className='search'>
-      
       <h1>{`PRODUCTS ACCORDING TO YOUR SEARCH: '${name.toUpperCase()}'`}</h1>
-      {products.length > 0 ? (
-        products.map((product) => <Card key={product.id} id={product.id} className='search__card' />)
-      ) : (
-        <p>Your search has no results. Try with a different keyword.</p>
-      )}
+      <div className='search__container'>
+        {products.length > 0 ? (
+          
+          products.map((product) => <Card key={product.id} id={product.id} className='search__container__card' />)
+        ) : (
+          <p>Your search has no results. Try with a different keyword.</p>
+        )}
+      </div>
     </main>
   )
 }
