8ef9bd126e15db7461bfce7fbd718842c1cebe4d
1106 <sanders.gesture-0c@icloud.com>
2023-12-09 19:30:35 -0600

    style improved

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import './footer.sass'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '@/assets/img/Xmart-logo.svg'
 import { useScreenContext } from '@/Context/ScreenContext'
 import { BsCart4, BsFillPersonFill } from 'react-icons/bs'
 
@@ -12,38 +13,46 @@ const Footer = () => {
     <>
       {screenSize >= 481 && (
         <footer className='footer'>
-          <section>
+          <section aria-label="Get to know us">
             <Link to='https://github.com/Xarenmi' className='footer__link'>
               <h4>Get to know us</h4>
             </Link>
           </section>
-          <section>
+          <section aria-label="Make money with us">
             <Link to='/sell' className='footer__link'>
               <h4>
                 Make money with us
               </h4>
             </Link>
           </section>
-          <section>
+          <section aria-label="Need assistance?">
             <Link to='/help' className='footer__link'>
               <h4>Need assistance?</h4>
             </Link>
           </section>
-        </footer>)}
+        </footer>
+      )}
 
       {screenSize <= 480 && (
         <footer className='footer'>
           <section>
+            <Link to='/' className='footer__link'>
+            <img
+              className='footer__logo'
+              src={logo}
+              alt='Xmart Logo'
+            />
+          </Link>
           </section>
 
-          <section>
+          <section aria-label="Login">
             <Link to='/login'>
-              <BsFillPersonFill className='footer__login' />
+              <BsFillPersonFill className='footer__login' aria-label="Login" />
             </Link>
           </section>
 
-          <section>
-            <Link to='/cart' className='footer__cart' >
+          <section aria-label="Cart with item count">
+            <Link to='/cart' className='footer__cart'>
               <p>{cartItems.length}</p>
               <p><BsCart4 /></p>
             </Link>
@@ -54,4 +63,4 @@ const Footer = () => {
   );
 };
 
-export default Footer
+export default Footer;
