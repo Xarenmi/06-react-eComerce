d4b6dd7cc098df10cbbcc5db253a34877a341508
1106 <sanders.gesture-0c@icloud.com>
2023-12-09 19:55:08 -0600

    responsive implemented

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
 import './header.sass'
-import logo from '@/assets/img/Xmart-logo.svg'
 import { Link } from 'react-router-dom'
+import logo from '@/assets/img/Xmart-logo.svg'
 import SearchBar from '@/Components/SearchBar/SearchBar'
+import { useScreenContext } from '@/Context/ScreenContext'
 import { BsCart4, BsFillPinMapFill, BsFillPersonFill } from 'react-icons/bs'
 
 const Header = () => {
+  const { screenSize } = useScreenContext()
+
   const cartItems = [1, 2, 3, 4]
   const client = {
     name: 'Xarenmi',
@@ -13,62 +16,69 @@ const Header = () => {
     location: 'worldwide'
   }
 
-  function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } 
-  }
+  //getLocation function 
 
+  let navCategories
+  screenSize <= 480 ? 
+  navCategories =  ['women', 'men', 'gadgets', 'automotive', 'home-decoration'] :
+  navCategories =  ['women', 'men', 'gadgets', 'automotive', 'home-decoration', 'skincare', 'groceries']
   
 
-  function showPosition(position) {
-    client.locationY = position.coords.latitude
-    client.locationX = position.coords.longitude
-  }
-
-  const navCategories = ['women', 'men', 'gadgets', 'automotive', 'home-decoration', 'skincare', 'groceries']
-
   return (
-    <header className='header'>
-      <section className='top'>
-        <div className='top__logoContainer'>
-          <Link to='/' className='top__logoLink'>
-            <img
-              className='top__logo'
-              src={logo}
-              alt='Xmart Logo'
-            />
-            <p>Xmart</p>
-          </Link>
-        </div>
+    <>
+      {screenSize >= 481 && (
+        <header className='header'>
 
-        <div className='top__deliveryRegion' aria-label='Delivery region'>
-          <p>Ship to:</p>
-          <BsFillPinMapFill />  {client.location}
-        </div>
+          <section className='top'>
 
-        <SearchBar />
+            <div className='top__logoContainer'>
+              <Link to='/' className='top__logoLink'>
+                <img
+                  className='top__logo'
+                  src={logo}
+                  alt='Xmart Logo'
+                />
+                <p>Xmart</p>
+              </Link>
+            </div>
+            <div className='top__deliveryRegion' aria-label='Delivery region'>
+              <p>Ship to:</p>
+              <BsFillPinMapFill />  {client.location}
+            </div>
 
-        <Link to='/login'>
-          <BsFillPersonFill className='top__login' />
-          {/* <button className='top__loginButton' aria-label='Log in'>
-            Log In
-          </button> */}
-        </Link>
+            <SearchBar />
 
-        <div className='top__cart' aria-label='Cart'>
-          <Link to='/cart'>
-            <p>{cartItems.length}</p>
-            <p><BsCart4 className='' /></p>
-          </Link>
-        </div>
-      </section>
+            <Link to='/login'>
+              <BsFillPersonFill className='top__login' />
+            </Link>
 
-      <nav className='navbar' aria-label='Category navigation'>
-        {navCategories.map((cat, index) => <Link to={`/category/${cat}`} key={index}> <button className='navbar__button'>{cat.toUpperCase().replace(/-/g, ' ')}</button> </Link>)}
-      </nav>
-    </header>
+            <div className='top__cart' aria-label='Cart'>
+              <Link to='/cart'>
+                <p>{cartItems.length}</p>
+                <p><BsCart4 className='' /></p>
+              </Link>
+            </div>
+          </section>
 
+          <nav className='navbar' aria-label='Category navigation'>
+            {navCategories.map((cat, index) => <Link to={`/category/${cat}`} key={index}> <button className='navbar__button'>{cat.toUpperCase().replace(/-/g, ' ')}</button> </Link>)}
+          </nav>
+
+        </header>
+      )}
+
+      {screenSize <= 480 && (
+        <header>
+          <section className='top'>
+            <SearchBar />
+          </section>
+          <nav className='navbar' aria-label='Category navigation'>
+            {navCategories.map((cat, index) => <Link to={`/category/${cat}`} key={index}> <button className='navbar__button'>{cat.toUpperCase().replace(/-/g, ' ')}</button> </Link>)}
+          </nav>
+
+        </header>
+      )}
+    </>
   )
 }
 
