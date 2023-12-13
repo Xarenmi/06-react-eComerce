ff61d945cb2ee19108d92bfb4b75942d75f2a290
1106 <sanders.gesture-0c@icloud.com>
2023-12-13 08:53:15 -0600

    navigation adjustments p3

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -3,19 +3,16 @@ import { Outlet } from 'react-router-dom'
 import { Header, Footer } from '@/Components'
 import { ScreenProvider } from '@/Context/ScreenContext'
 import { ProductProvider } from '@/Context/ProductsContext'
-import { NavigationProvider } from '@/Context/NavigationContext'
 
 const Root = () => {
   return (
     <>
       <ScreenProvider>
-        <NavigationProvider>
           <ProductProvider >
             <Header />
             <Outlet />
             <Footer />
           </ProductProvider>
-        </NavigationProvider>
       </ScreenProvider>
     </>
   )
