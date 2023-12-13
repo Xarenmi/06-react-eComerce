90d22d449de014c9db6d95e04bcd4db23d3220e4
1106 <sanders.gesture-0c@icloud.com>
2023-12-13 10:53:21 -0600

    routes adjusted

--- src/Routes/Root.jsx
+++ src/Routes/Root.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header, Footer } from '@/Components'
-import { ScreenProvider } from '@/Context/ScreenContext'
-import { ProductProvider } from '@/Context/ProductsContext'
 
 const Root = () => {
   return (
     <>
-      <ScreenProvider>
-          <ProductProvider >
             <Header />
             <Outlet />
             <Footer />
-          </ProductProvider>
-      </ScreenProvider>
     </>
   )
 }
--- src/Routes/RouterIndex.jsx
+++ src/Routes/RouterIndex.jsx
@@ -1,21 +1,28 @@
-import { Routes, Route } from 'react-router-dom'
-import { AddProduct, Assistance, Cart, Category, Home, Product, Search } from '@/Pages'
 import Root from './Root'
+import { Routes, Route } from 'react-router-dom'
+import { ScreenProvider } from '@/Context/ScreenContext'
+import { ProductProvider } from '@/Context/ProductsContext'
+import { AddProduct, Assistance, Cart, Category, Home, Product, Search } from '@/Pages'
+
 
 export default function RouterIndex() {
     return (
-        <Routes>
-            <Route path="/" element={<Root />}>
-                <Route path="home" element={<Home />} />
-                <Route path="cart" element={<Cart />} />
-                <Route path="search/" element={<Search />} />
-                <Route path="sell" element={<AddProduct />} />
-                <Route path="help" element={<Assistance />} />
-                <Route path="search/:name" element={<Search />} />
-                <Route path="product/:title" element={<Product />} />
-                <Route path="category/:name" element={<Category />} />
-                <Route index element={<Home />} />
-            </Route>
-        </Routes>
+        <ScreenProvider>
+            <ProductProvider >
+                <Routes>
+                    <Route path="/" element={<Root />}>
+                        <Route path="home" element={<Home />} /> 
+                        <Route path="cart" element={<Cart />} />
+                        <Route path="search/" element={<Search />} />
+                        <Route path="sell" element={<AddProduct />} />
+                        <Route path="help" element={<Assistance />} />
+                        <Route path="search/:name" element={<Search />} />
+                        <Route path="product/:title" element={<Product />} />
+                        <Route path="category/:name" element={<Category />} />
+                        <Route index element={<Home />} />
+                    </Route>
+                </Routes>
+            </ProductProvider>
+        </ScreenProvider>
     );
 }
