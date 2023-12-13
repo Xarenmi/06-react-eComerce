d577101273fe747d06fdd297ca287e120fb67cfa
1106 <sanders.gesture-0c@icloud.com>
2023-12-12 22:39:39 -0600

    product styled

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useProductContext } from '@/Context/ProductsContext'
+import './product.sass'
 
 const Product = () => {
   const { productList } = useProductContext()
-  console.log(productList)
   let { title } = useParams()
   const thisProduct = productList.find((product) => product.title === title)
   if (!thisProduct) {
-    console.warn(`Product with title '${title}' not found.`);
-  } else {
-    console.log(thisProduct);
+    return (<p>Your search has no results. Try with a different keyword.</p>)
   }
 
+  const [selectedImage, setSelectedImage] = useState(thisProduct.images[0])
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
   return (
-    <main className='product'>  
-      {/* <p>{thisProduct.category}</p> */}
-      <h1>{thisProduct.title}</h1>
-      <p>{thisProduct.description}</p>
-      <p>{thisProduct.price}</p>
-      <p>{thisProduct.discountPercentage}</p>
-      <p>{thisProduct.rating}</p>
-      <p>{thisProduct.brand}</p>
-      {/*images, id?*/}
+    <main className='product'>
+      <div className="product-gallery">
+        <div className="main-image">
+          <img src={selectedImage} alt={`Product: ${thisProduct.title}`} />
+        </div>
+
+        <div className="thumbnail-gallery">
+          {thisProduct.images.map((image, index) => (
+            <div
+              key={index}
+              className={`gallery-item ${selectedImage === image ? 'active' : ''}`}
+              onClick={() => handleImageClick(image)}
+            >
+              <img src={image} alt={`Thumbnail ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='product-details'>
+        <h1>{thisProduct.title}</h1>
+        {/* Rating as yellow stars */}
+        <p className="rating-label">Rating: {Array.from({ length: 5 }).map((_, index) => (
+          <span key={index} className="star">⭐</span>
+        ))}</p>
+        <p>{thisProduct.description}</p>
+        {/* Discount label */}
+        <p className="discount-label">Discount: -{thisProduct.discountPercentage}%</p>
+        {/* Add to cart button */}
+        <p>$ {thisProduct.price} <br/> <button className='add-to-cart'>Add to cart</button></p>
+        
+      </div>
     </main>
   )
 }
